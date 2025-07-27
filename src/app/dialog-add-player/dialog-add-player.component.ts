721540bad0ac1ea58774294a33ebce49527e6ad1
Noah Velickovic <noah.velic@hotmail.com>
2025-07-27 19:56:47 +0200

    implementation changing characters

---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-dialog-add-player',
+  standalone: true,
+  imports: [MatDialogModule, MatIconModule, CommonModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule],
+  templateUrl: './dialog-add-player.component.html',
+  styleUrl: './dialog-add-player.component.scss'
+})
+export class DialogAddPlayerComponent {
+name: string = '';
+ onNoClick(): void {
+    
+}
+}
